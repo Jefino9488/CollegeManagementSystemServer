9fbddc5c6421a18552f18bd8ba4c536460c3c9ea
Jefino9488 <jefinojacob9488@gmail.com>
2026-01-06 09:22:36 +0530

    Add Gradle wrapper scripts and configuration files

---
+++ src/main/java/org/jefino/cms/cmsplatform/CollegeManagementSystemServerApplication.java
@@ -0,0 +1,13 @@
+package org.jefino.cms.cmsplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollegeManagementSystemServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CollegeManagementSystemServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/jefino/cms/cmsplatform/CollegeManagementSystemServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.jefino.cms.cmsplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollegeManagementSystemServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
